e79af719779636f7a3798392e175a7e73a0f851e
fadil_abdi <mohfadilabdillah1302@gmail.com>
2022-11-22 20:49:02 +0700

    sudah di join

--- navigasi/daftar_akun.php
+++ navigasi/daftar_akun.php
@@ -2,14 +2,29 @@
 <div class="daftar_akun">
     <?php
     include ('koneksi.php');
-    $home_admin = $kon -> prepare("select * from nasabah");
-    $home_admin -> execute(); 
+    $home_admin = $kon -> prepare("select nasabah.*,rekening.SALDO_REK from nasabah inner join rekening ON nasabah.USERNAME_NSB = rekening.USERNAME_NSB");
+    $home_admin -> execute();
     foreach($home_admin as $data){
-        echo "{$data['NAMA_NSB']}";
-        echo "{$data['ALAMAT_NSB']}";
-        echo "{$data['EMAIL_NSB']} ";
-        echo "{$data['TGL_NSB']} ";
-        echo "{$data['NO_HP_NSB']} ";
+    ?>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Email</th>
+            <th>Tanggal Pembuatan</th>
+            <th>No Hp</th>
+            <th>Saldo</th>
+        </tr>
+        <tr>
+            <td><?php echo "{$data['NAMA_NSB']}"; ?></td>
+            <td><?php echo "{$data['ALAMAT_NSB']}"; ?></td>
+            <td><?php echo "{$data['EMAIL_NSB']} "; ?></td>
+            <td><?php echo "{$data['TGL_NSB']} "; ?></td>
+            <td><?php echo "{$data['NO_HP_NSB']} "; ?></td>
+            <td><?php echo "{$data['SALDO_REK']} "; ?></td>
+        </tr>
+    </table>
+    <?php
     }
     ?>
 </div>
