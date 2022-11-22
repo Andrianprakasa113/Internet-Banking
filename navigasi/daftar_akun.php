12e91de3b58abea270c327072f65f020c0de5581
fadil_abdi <mohfadilabdillah1302@gmail.com>
2022-11-22 19:51:41 +0700

    nambah tampilan

--- navigasi/daftar_akun.php
+++ navigasi/daftar_akun.php
@@ -1,5 +1,16 @@
 <div class="scroller">
 <div class="daftar_akun">
-    <h1>INI daftar akun</h1>
+    <?php
+    include ('koneksi.php');
+    $home_admin = $kon -> prepare("select * from nasabah");
+    $home_admin -> execute(); 
+    foreach($home_admin as $data){
+        echo "{$data['NAMA_NSB']}";
+        echo "{$data['ALAMAT_NSB']}";
+        echo "{$data['EMAIL_NSB']} ";
+        echo "{$data['TGL_NSB']} ";
+        echo "{$data['NO_HP_NSB']} ";
+    }
+    ?>
 </div>
 </div>
