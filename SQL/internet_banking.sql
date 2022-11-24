6daffc60ac42377effeb1e7b054f6fc9b04e81e3
fadil_abdi <mohfadilabdillah1302@gmail.com>
2022-11-24 07:43:18 +0700

    ok

--- SQL/internet_banking.sql
+++ SQL/internet_banking.sql
@@ -1,27 +1,54 @@
--- Adminer 4.8.1 MySQL 10.4.24-MariaDB dump
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2022 at 01:36 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `internet_banking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
 CREATE TABLE `admin` (
   `USERNAME_ADMIN` varchar(35) NOT NULL,
   `SANDI_ADMIN` varchar(70) DEFAULT NULL,
   `NAMA_ADMIN` varchar(35) DEFAULT NULL,
   `EMAIL_ADMIN` varchar(35) DEFAULT NULL,
-  `NO_HP_ADMIN` decimal(14,0) DEFAULT NULL,
-  PRIMARY KEY (`USERNAME_ADMIN`)
+  `NO_HP_ADMIN` decimal(14,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `admin` (`USERNAME_ADMIN`, `SANDI_ADMIN`, `NAMA_ADMIN`, `EMAIL_ADMIN`, `NO_HP_ADMIN`) VALUES
-('ad',	'123',	'andre',	'andre@gmail.com',	81),
-('admin',	'123',	'andre',	'andre@gmail.com',	81);
+--
+-- Dumping data for table `admin`
+--
 
-DROP TABLE IF EXISTS `nasabah`;
+INSERT INTO `admin` (`USERNAME_ADMIN`, `SANDI_ADMIN`, `NAMA_ADMIN`, `EMAIL_ADMIN`, `NO_HP_ADMIN`) VALUES
+('ad', '123', 'andre', 'andre@gmail.com', '81'),
+('admin', '123', 'andre', 'andre@gmail.com', '81');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nasabah`
+--
+
 CREATE TABLE `nasabah` (
   `USERNAME_NSB` varchar(35) NOT NULL,
   `USERNAME_ADMIN` varchar(35) DEFAULT NULL,
@@ -30,37 +57,105 @@ CREATE TABLE `nasabah` (
   `ALAMAT_NSB` text DEFAULT NULL,
   `EMAIL_NSB` varchar(35) DEFAULT NULL,
   `TGL_NSB` date DEFAULT NULL,
-  `NO_HP_NSB` decimal(14,0) DEFAULT NULL,
-  PRIMARY KEY (`USERNAME_NSB`),
-  KEY `FK_MENGATUR` (`USERNAME_ADMIN`),
-  CONSTRAINT `FK_MENGATUR` FOREIGN KEY (`USERNAME_ADMIN`) REFERENCES `admin` (`USERNAME_ADMIN`)
+  `NO_HP_NSB` decimal(14,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `nasabah`
+--
 
-DROP TABLE IF EXISTS `rekening`;
+INSERT INTO `nasabah` (`USERNAME_NSB`, `USERNAME_ADMIN`, `PASSWORD_NSB`, `NAMA_NSB`, `ALAMAT_NSB`, `EMAIL_NSB`, `TGL_NSB`, `NO_HP_NSB`) VALUES
+('fadil12', NULL, '223', 'fadil', 'bangkalan', 'fadil@gg.com', '2022-11-15', '122300');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rekening`
+--
+
 CREATE TABLE `rekening` (
   `USERNAME_NSB` varchar(35) NOT NULL,
   `NO_REK` decimal(12,0) NOT NULL,
   `WAKTU_BUAT_REK` date DEFAULT NULL,
-  `SALDO_REK` float(15,2) DEFAULT NULL,
-  PRIMARY KEY (`USERNAME_NSB`,`NO_REK`),
-  KEY `NO_REK` (`NO_REK`),
-  CONSTRAINT `FK_MEMILIKI` FOREIGN KEY (`USERNAME_NSB`) REFERENCES `nasabah` (`USERNAME_NSB`)
+  `SALDO_REK` decimal(11,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `rekening`
+--
 
-DROP TABLE IF EXISTS `transaksi`;
+INSERT INTO `rekening` (`USERNAME_NSB`, `NO_REK`, `WAKTU_BUAT_REK`, `SALDO_REK`) VALUES
+('fadil12', '203324102491', '2022-11-02', '1000000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
 CREATE TABLE `transaksi` (
   `WAKTU_TRANSAKSI` datetime NOT NULL,
-  `JUM_TRANSFER` float(15,2) DEFAULT NULL,
+  `JUM_TRANSFER` float(11,2) DEFAULT NULL,
   `no_rek_pengirim` decimal(12,0) NOT NULL,
-  `no_rek_penerima` decimal(12,0) NOT NULL,
-  PRIMARY KEY (`WAKTU_TRANSAKSI`),
-  KEY `no_rek_pengirim` (`no_rek_pengirim`),
-  KEY `no_rek_penerima` (`no_rek_penerima`),
-  CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`no_rek_pengirim`) REFERENCES `rekening` (`NO_REK`),
-  CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`no_rek_penerima`) REFERENCES `rekening` (`NO_REK`)
+  `no_rek_penerima` decimal(12,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexes for dumped tables
+--
 
--- 2022-11-19 18:13:42
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`USERNAME_ADMIN`);
+
+--
+-- Indexes for table `nasabah`
+--
+ALTER TABLE `nasabah`
+  ADD PRIMARY KEY (`USERNAME_NSB`),
+  ADD KEY `FK_MENGATUR` (`USERNAME_ADMIN`);
+
+--
+-- Indexes for table `rekening`
+--
+ALTER TABLE `rekening`
+  ADD PRIMARY KEY (`NO_REK`,`USERNAME_NSB`) USING BTREE,
+  ADD KEY `USERNAME_NSB` (`USERNAME_NSB`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`WAKTU_TRANSAKSI`),
+  ADD KEY `no_rek_pengirim` (`no_rek_pengirim`),
+  ADD KEY `no_rek_penerima` (`no_rek_penerima`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `nasabah`
+--
+ALTER TABLE `nasabah`
+  ADD CONSTRAINT `FK_MENGATUR` FOREIGN KEY (`USERNAME_ADMIN`) REFERENCES `admin` (`USERNAME_ADMIN`);
+
+--
+-- Constraints for table `rekening`
+--
+ALTER TABLE `rekening`
+  ADD CONSTRAINT `FK_MEMILIKI` FOREIGN KEY (`USERNAME_NSB`) REFERENCES `nasabah` (`USERNAME_NSB`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`no_rek_pengirim`) REFERENCES `rekening` (`NO_REK`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`no_rek_penerima`) REFERENCES `rekening` (`NO_REK`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
