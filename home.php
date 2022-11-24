b0e5f0db0342262222ee703eb38d90d09e45d4e4
Frosi-m <fahrurrosi380@gmail.com>
2022-11-24 10:46:54 +0700

    a

--- home.php
+++ home.php
@@ -76,6 +76,9 @@ if (isset($_SESSION['admin']) || isset($_SESSION['nsb'])) {
             else if(@$_GET['link_edit_profile'] == 'edit' && isset($_SESSION['nsb'])){
                 include 'navigasi/edit_profile_nsb.php';
             }
+            else if(@$_GET['link_edit_profile'] != ' ' && isset($_SESSION['admin'])){
+                include 'navigasi/edit_profile_nsb.php';
+            }
             else if (@$_GET['link'] == '' && (isset($_SESSION['admin']) || isset($_SESSION['nsb']))){
                 include 'navigasi/homeAplikasi.php';
             }
