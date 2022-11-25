7188165e1992af3b54f27393fd00eb3016eee210
Frosi-m <fahrurrosi380@gmail.com>
2022-11-25 20:48:26 +0700

    a

--- navigasi/melakukan_transaksi.php
+++ navigasi/melakukan_transaksi.php
@@ -95,33 +95,22 @@ if (isset($tombol) && $rekening_asal === true && $rekening_penerima === true && 
                     //sekarang eksekusi pogramnya
                     if ((int) $cek_saldo['SALDO_REK'] >= $_POST['jumlah_tf']) {
                             //ambil dulu data saldo dari si pengirim
-                                $kalimat_query = $kon -> prepare('SELECT SALDO_REK from rekening where NO_REK = :no_rek');
-                                $kalimat_query -> bindValue(':no_rek',  $_POST['rekening_asal']);
-                                $kalimat_query -> execute(); 
-                                $saldo_pengirim = $kalimat_query -> fetch();
+                                $saldo_pengirim = cek_saldo($_POST['rekening_asal']);
                                 //lalu kurangi saldo_pengirim dari pengirim 
                                 $rumus_pengirim = $saldo_pengirim['SALDO_REK'] - $_POST['jumlah_tf'];
 
-                                //update data rekening pengirim
-                                $kalimat_query = $kon -> prepare('UPDATE rekening SET SALDO_REK = :update_saldo where NO_REK = :no_rek');
-                                $kalimat_query -> bindValue(':no_rek', $_POST['rekening_asal']);
-                                $kalimat_query -> bindValue(':update_saldo', $rumus_pengirim);
-                                $kalimat_query -> execute();
 
-                                //ambil dulu data saldo dari si penerima
-                                $kalimat_query = $kon -> prepare('SELECT SALDO_REK from rekening where NO_REK = :no_rek');
-                                $kalimat_query -> bindValue(':no_rek', $_POST['rekening_penerima']);
-                                $kalimat_query -> execute(); 
-                                $saldo_penerima =  $kalimat_query -> fetch();
+                                //update data rekening pengirim
+                                update_saldo($_POST['rekening_asal'], $rumus_pengirim);
+
+                                //ambil dulu data saldo dari si penerima 
+                                $saldo_penerima =  $saldo_pengirim = cek_saldo($_POST['rekening_penerima']);
 
                                 //lalu tambah saldo_penerima dari pengirim
                                 $rumus_penerima = $saldo_penerima['SALDO_REK'] + $_POST['jumlah_tf'];
 
                                 //update data rekening pengirim
-                                $kalimat_query = $kon -> prepare('UPDATE rekening SET SALDO_REK = :update_saldo where NO_REK = :no_rek');
-                                $kalimat_query -> bindValue(':no_rek',$_POST['rekening_penerima']);
-                                $kalimat_query -> bindValue(':update_saldo', $rumus_penerima);
-                                $kalimat_query -> execute();
+                                update_saldo($_POST['rekening_penerima'], $rumus_penerima);
                                
                                 //tambahkan data transaksi
                                 $kalimat_query = $kon -> prepare('INSERT INTO transaksi (WAKTU_TRANSAKSI,JUM_TRANSFER,no_rek_pengirim,no_rek_penerima) VALUES (now(),:total_tf,:si_pengirim,:si_penerima)');
